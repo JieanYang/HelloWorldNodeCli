b905169ef0ee069d8f7e0c8dc17afda381138373
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-04 15:58:43 +0100

    Downgrade chalk from 5.1.2 to 4.1.2

--- src/cli.ts
+++ src/cli.ts
@@ -1,4 +1,7 @@
 #!/usr/bin/env node
+
+import chalk from 'chalk';
+
 const main = (argv: string[]) => {
 	const program = require('commander');
 	program.option('-v, --verbose', 'show debug log');
@@ -7,6 +10,7 @@ const main = (argv: string[]) => {
 
 	const options = program.opts();
 	if (options.verbose) console.log(options);
+	console.log(chalk.red('this will be red text'));
 };
 
 main(process.argv);
