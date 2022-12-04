41add31eaa4e78dab624bc33889dc072a33525e2
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-04 17:38:20 +0100

    Update demo

--- index.ts
+++ index.ts
@@ -7,24 +7,27 @@
  * @author Jiean Yang <https://wemediaweb.com/>
  */
 
+// import { callChucknorris } from './utils/TCall';
+
 const init = require('./utils/init.ts');
 const cli = require('./utils/cli');
 const log = require('./utils/log');
-const axios = require('axios');
 
 const input = cli.input;
 const flags = cli.flags;
 const { clear, debug } = flags;
 
-(async () => {
+const main = async () => {
 	init({ clear });
 	console.log('input', input);
 	input.includes(`help`) && cli.showHelp(0);
 
-	if (input.includes('joke')) {
-		const res = await axios.get('https://api.chucknorris.io/jokes/random');
-		console.log(res.data);
-	}
+	// if (input.includes('joke')) {
+	// 	const data = await callChucknorris();
+	// 	console.log(data);
+	// }
 
 	debug && log(flags);
-})();
+};
+
+main();
