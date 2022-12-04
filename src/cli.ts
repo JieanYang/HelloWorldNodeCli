81bb9082e45551d4be04631fa2ef62c9ccfefe5f
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-04 15:51:51 +0100

    UIpdate cli.ts

--- src/cli.ts
+++ src/cli.ts
@@ -1,18 +1,12 @@
 #!/usr/bin/env node
 const main = (argv: string[]) => {
 	const program = require('commander');
-	program
-		.option('-d, --debug', 'output extra debugging')
-		.option('-s, --small', 'small pizza size')
-		.option('-p, --pizza-type <type>', 'flavour of pizza');
+	program.option('-v, --verbose', 'show debug log');
 
 	program.parse(argv);
 
 	const options = program.opts();
-	if (options.debug) console.log(options);
-	console.log('pizza details:');
-	if (options.small) console.log('- small pizza size');
-	if (options.pizzaType) console.log(`- ${options.pizzaType}`);
+	if (options.verbose) console.log(options);
 };
 
 main(process.argv);
