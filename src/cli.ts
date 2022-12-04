61d423cda5f4e5c8a38996055f2ab3dc5a0cd47d
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-04 15:48:40 +0100

    Install commander an apply demo code

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,18 @@
 #!/usr/bin/env node
 const main = (argv: string[]) => {
-	console.log('yang', argv);
+	const program = require('commander');
+	program
+		.option('-d, --debug', 'output extra debugging')
+		.option('-s, --small', 'small pizza size')
+		.option('-p, --pizza-type <type>', 'flavour of pizza');
+
+	program.parse(argv);
+
+	const options = program.opts();
+	if (options.debug) console.log(options);
+	console.log('pizza details:');
+	if (options.small) console.log('- small pizza size');
+	if (options.pizzaType) console.log(`- ${options.pizzaType}`);
 };
 
-main(process.argv.slice(2));
+main(process.argv);
