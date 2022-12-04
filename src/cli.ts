22d917816684e942d7c206d6acbefefbdc6a6208
Jiean Yang <jiean.yang.freelancer@gmail.com>
2022-12-04 16:01:48 +0100

    Simplify codes

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 
-import chalk from 'chalk';
+import { red } from 'chalk';
 
 const main = (argv: string[]) => {
 	const program = require('commander');
@@ -10,7 +10,7 @@ const main = (argv: string[]) => {
 
 	const options = program.opts();
 	if (options.verbose) console.log(options);
-	console.log(chalk.red('this will be red text'));
+	console.log(red('this will be red text'));
 };
 
 main(process.argv);
